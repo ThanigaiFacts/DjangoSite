05492d7006c053fc715ee1c90bcf288d3fdc1a3a
thanigavel <thanigaisolutions@gmail.com>
2022-10-07 16:26:10 +0530

    initial setup

--- Django_Site/settings.py
+++ Django_Site/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +130,8 @@ STATICFILES_DIRS = (
     Path(BASE_DIR, 'templates/static'),
 )
 
-STATIC_ROOT = Path(BASE_DIR, 'static')
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
