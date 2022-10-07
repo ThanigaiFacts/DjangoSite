bffec6bb272427ec37de1c845fd037ca3dcd3d61
thanigavel <thanigaisolutions@gmail.com>
2022-10-07 16:35:48 +0530

    initial setup

---
+++ staticfiles/JS/security.js
@@ -0,0 +1,28 @@
+
+/*
+function preventBack() {
+       window.history.forward();
+    }
+setTimeout("preventBack()", 0);
+window.onunload = function () { null };
+*/
+  function DisableBackButton() {
+       window.history.forward()
+      }
+     DisableBackButton();
+     window.onload = DisableBackButton;
+     window.onpageshow = function(evt) { if (evt.persisted) DisableBackButton() }
+     window.onunload = function() { void (0) }
+
+
+$(document).bind("contextmenu",function(e){
+  show_alert("Right Click is Prevented");
+  return false;
+});
+
+$('body').bind('cut copy paste',function(e) {
+    e.preventDefault();
+    show_alert("You are not allowed to use Cut, Copy, Paste operations");
+    return false;
+});
+
