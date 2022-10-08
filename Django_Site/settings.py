525c3a61b301a4642b2bd4ed03657d15898efbc4
thanigavel <thanigaisolutions@gmail.com>
2022-10-08 11:18:17 +0530

    initial setup

--- Django_Site/settings.py
+++ Django_Site/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = ["thanigaifacts.herokuapp.com","127.0.0.1"]
 
