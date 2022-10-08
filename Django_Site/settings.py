eb942572212c6d8d052f9c80aba11b7f28d25575
thanigavel <thanigaisolutions@gmail.com>
2022-10-08 10:05:45 +0530

    initial setup

--- Django_Site/settings.py
+++ Django_Site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ["thanigaifacts.herokuapp.com/","127.0.0.1"]
+ALLOWED_HOSTS = ["thanigaifacts.herokuapp.com","127.0.0.1"]
 #ALLOWED_HOSTS = ["127.0.0.1"]
 
 
